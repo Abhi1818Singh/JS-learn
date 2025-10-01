97cfb23dc94a43cbdb971267867a2a1ed0d6e97e
Abhi1818Singh <singhabhishekindradeo@gmail.com>
2025-10-01 19:17:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treats all JS code as newer Version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3+
+    3
+);   // this you shouldn't do... code readability should be high
+
+let name ="abhishek"
+let age = 17
+let isLoggedIn = true
+let state;
+
+//datatypes
+//number -> 2 to the power 53
+//string -> " " preferable or ' '
+//bigint
+//boolean -> true/false
+//null -> standalone value
+// symbol -> unique
+
+//object
+
+console.log(typeof "abhishek")
+console.log(typeof age)
+console.log(typeof undefined)  // undefined
+console.log(typeof null)    //object 
