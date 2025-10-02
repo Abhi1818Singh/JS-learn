e7dd5b9db2f0cffd6efe7019d660381dfdde710f
Abhi1818fire <singhabhishekindradeo@gmail.com>
2025-10-02 19:03:17 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,4 +27,46 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
+//****************************Operations****************************
 
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+str1 ="hello"
+str2 =" Abhishek"
+str3 = str1 + str2
+
+console.log(str3);
+
+// this is okay but problem arises here
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);   returns 1 but this is very bad way to write code
+// console.log(+""); returns 0
+
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //again not a better way to write 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
